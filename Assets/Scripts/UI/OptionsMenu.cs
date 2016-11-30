a3f210c4b863fec83c3129cb33051b5ae1347fd7
Stuart Aaron Millman <stu.millman15@gmail.com>
2016-11-30 14:41:22 +0000

    Bug Fix: Game now default to 1280x720 res if resolution is invalid
    - Also moved Footer Version label to left so that it is not behind the "Development Build" label Unity adds

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -47,21 +47,26 @@ public class OptionsMenu : MonoBehaviour {
 	public void LoadSettings() {
 		int width = PlayerPrefs.GetInt(DISPLAY_WIDTH);
 		int height = PlayerPrefs.GetInt(DISPLAY_HEIGHT);
-		bool isFullscreen = (PlayerPrefs.GetInt(DISPLAY_FULLSCREEN) == 1 ? true : false);
-		Screen.SetResolution(width, height, isFullscreen);
+		bool isFullscreen = (PlayerPrefs.GetInt(DISPLAY_FULLSCREEN) == 1);
+		if(ValidResolution(width, height))
+			Screen.SetResolution(width, height, isFullscreen);
+		else
+			Screen.SetResolution(1280, 720, false); //If a invalid resoltion has been saved default to 1280x720
+
+
 	}
 
 	public void ApplySettings() {
-		//Resolution
+		//Resolution & Window Mode
 		string res = ResolutionSelector.GetValue();
 		int width = int.Parse(res.Substring(0, res.IndexOf("x", StringComparison.Ordinal)));
-		int height = int.Parse(res.Substring(res.IndexOf("x", StringComparison.Ordinal)));
+		int height = int.Parse(res.Substring(res.IndexOf("x", StringComparison.Ordinal) + 1));
 		PlayerPrefs.SetInt(DISPLAY_WIDTH, width);
 		PlayerPrefs.SetInt(DISPLAY_HEIGHT, height);
-
-		//Window Mode
 		int isFullscreen = (WindowModeSelector.GetValue() != "Windowed") ? 1 : 0;
 		PlayerPrefs.SetInt(DISPLAY_FULLSCREEN, isFullscreen);
+		PlayerPrefs.SetInt(DISPLAY_HEIGHT, height);
+		Screen.SetResolution(width, height, (isFullscreen == 1));
 
 		//Main Volume
 		float mainVolume = Main.value;
@@ -91,6 +96,15 @@ public class OptionsMenu : MonoBehaviour {
 	private void ResetWindowModeSelector() {
 		bool isFullscreen = Screen.fullScreen;
 		WindowModeSelector.SetSelectedIndex((isFullscreen ? 1 : 0)); // Options should be 0="Windowed" then 1="Borderless Windowed"
+	}
+
+	private bool ValidResolution(int width, int height) {
+		foreach(Resolution res in Screen.resolutions) {
+			if(res.width == width && res.height == height)
+				return true;
+		}
+
+		return false;
 	}
 
 }
