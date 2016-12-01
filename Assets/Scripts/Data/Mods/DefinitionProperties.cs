ae1b0fd2074229122a33ea96b59b808cdb762cb4
Stuart Aaron Millman <stu.millman15@gmail.com>
2016-12-01 08:37:01 +0000

    Changed DefinitionProperties to now store definitions assembly & BuildMethod is determined in XML Def

--- Assets/Scripts/Data/Mods/DefinitionProperties.cs
+++ Assets/Scripts/Data/Mods/DefinitionProperties.cs
@@ -1,6 +1,12 @@
+using System;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
 using System.Xml;
+using Microsoft.CSharp;
 using UnityEngine;
 
 public class DefinitionProperties {
@@ -8,14 +14,24 @@ public class DefinitionProperties {
 	public string DefName { get; protected set; }
 	public string DefCategory { get; protected set; }
 
+	public bool HasAssembly;
+	private string AssemblyFile; //The path to this defintions Assembly file
+	public Assembly Assembly { get; protected set; } // The Assembly created from AssemblyFile
+	public string ClassName { get; protected set; } // The name of the Class that belongs to this definition
+
 	protected Dictionary<string, XMLTag> XMLData;
 
-	public DefinitionProperties(XmlNode defNode) {
+	public DefinitionProperties(XmlNode defNode, Definition def) {
 		XMLData = new Dictionary<string, XMLTag>();
 		XMLDefToDataStructure(defNode);
 
 		this.DefName = XMLData["DefName"].Value;
 		this.DefCategory = XMLData["Category"].Value;
+
+		if(XMLData.ContainsKey("Code")) {
+			XMLTag codeTag = XMLData["Code"];
+			LoadAssembly(def.GetMod().RootDir + "/Assemblies/" + codeTag.Attributes["assembly"], codeTag.Attributes["class"]);
+		}
 	}
 
 	private void XMLDefToDataStructure(XmlNode defNode) {
@@ -53,6 +69,75 @@ public class DefinitionProperties {
 		}
 
 		return xmlDataTag;
+	}
+
+	private void LoadAssembly(string assemblyFilePath, string className) {
+		Assembly assembly = null;
+
+		FileInfo assemblyFile = new FileInfo(assemblyFilePath);
+		if(assemblyFile.Extension == ".cs")
+			assembly = CompileCode(File.ReadAllText(assemblyFilePath));
+		else
+			assembly = AssemblyFromDLL(assemblyFilePath);
+
+		if(assembly == null) {
+			Debug.LogError("DefinitionProperties::LoadAssembly -> Assembly is null!");
+			return;
+		}
+
+		this.HasAssembly = true;
+		this.AssemblyFile = assemblyFilePath;
+		this.ClassName = className;
+		this.Assembly = assembly;
+	}
+
+	private Assembly CompileCode(string source) {
+		CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+		CompilerParameters param = new CompilerParameters();
+
+		foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+			param.ReferencedAssemblies.Add(assembly.Location);
+		}
+
+		//Generate Dll in Memory
+		param.GenerateExecutable = false;
+		param.GenerateInMemory = true;
+
+		CompilerResults result = codeProvider.CompileAssemblyFromSource(param, source);
+		if(result.Errors.Count > 0) {
+			StringBuilder builder = new StringBuilder();
+			foreach(CompilerError error in result.Errors) {
+				builder.AppendFormat("Error ({0}): {1}\n", error.ErrorNumber, error.ErrorText);
+			}
+			throw new Exception(builder.ToString());
+		}
+
+		Debug.Log("Definition -> Assembly Loaded: " + AssemblyFile);
+
+		//Return Assembly
+		return result.CompiledAssembly;
+	}
+
+	private Assembly AssemblyFromDLL(string dllFile) {
+		Assembly assembly = Assembly.LoadFile(dllFile);
+		if(assembly == null)
+			Debug.LogError("Definition -> Couldn't load DLL: " + dllFile);
+
+		Debug.Log("Definition -> Assembly Loaded: " + AssemblyFile);
+		return assembly;
+	}
+
+	public object CreateAssemblyClassInstance() {
+		object instance = Assembly.CreateInstance(ClassName);
+		return instance;
+	}
+
+	public string GetValue(string key) {
+		return this.XMLData[key].Value;
+	}
+
+	public Type GetAssemblyClassType() {
+		return Assembly.GetType(ClassName);
 	}
 
 	public class XMLTag {
